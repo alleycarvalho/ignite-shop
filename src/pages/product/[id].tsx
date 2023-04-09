25f84237859880ba11e280a74c7dd898de8a9985
Alley M. Carvalho <alleycarvalho@gmail.com>
2023-04-09 13:27:30 -0300

    feat: product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
 import { useRouter } from "next/router"
 
+import { ImageContainer, ProductContainer, ProductDetails } from "@styles/pages/product"
+
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias necessitatibus illo deserunt cupiditate. Reprehenderit eligendi animi accusantium distinctio dolorem autem minima, quisquam dolore voluptates assumenda odio, esse nihil, dolorum saepe?</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,6 +12,7 @@ export const globalStyles = globalCss({
   '*': {
     margin: 0,
     padding: 0,
+    boxSizing: 'border-box',
   },
 
   body: {
